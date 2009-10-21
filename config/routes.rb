d819e132411041c86e16765f3e6c529586212b7e
Neeraj Singh <neerajdotname@gmail.com>
2009-10-21 11:28:56 -0400

    work in progress

--- config/routes.rb
+++ config/routes.rb
@@ -25,31 +25,10 @@ ActionController::Routing::Routes.draw do |map|
                           :path_prefix => 'admin_data',
                           :controller => 'main',
                           :member => {:del => :delete},
-                          :collection => {:table_structure => :get}
+                          :collection => {:table_structure => :get, 
+                                          :delete_all => :post,
+                                          :destroy_all => :post}
 
   end
-
-  #map.with_options :controller => 'admin_data/main' do |m|
-
-    #m.admin_data_show               '/admin_data/show' ,              :action => 'show'
-
-    #m.admin_data_destroy            '/admin_data/destroy',            :action => 'destroy', 
-                                                                      #:conditions => {:method => :delete}
-
-    #m.admin_data_delete             '/admin_data/delete',             :action => 'delete', 
-                                                                      #:conditions => {:method => :delete}
-
-    #m.admin_data_edit               '/admin_data/edit',               :action => 'edit'
-    
-    #m.admin_data_update             '/admin_data/update',             :action => 'update', 
-                                                                      #:conditions => {:method => :post}
-
-    #m.admin_data_table_structure    '/admin_data/table_structure',    :action => 'table_structure'
-
-    #m.admin_data_new                '/admin_data/new',                :action => 'new'
-
-    #m.admin_data_create             '/admin_data/create',             :action => 'create', 
-                                                                      #:conditions => {:method => :post}
-  #end
 
 end
